4caf40cff05b7ed5518f66fb4a4b143047eaa897
0xOsiris <djosiris@proton.me>
2023-02-07 20:48:07 -0600

    Added calculate_virtual_reserve test

--- src/pool/uniswap_v3.rs
+++ src/pool/uniswap_v3.rs
@@ -1107,6 +1107,35 @@ mod test {
         assert_eq!(11218179125914784_u128, price_a_64_x);
         assert_eq!(30333119403920214119581_u128, price_b_64_x);
     }
+    #[tokio::test]
+    async fn test_calculate_virtual_reserves() {
+        let rpc_endpoint = std::env::var("ETHEREUM_MAINNET_ENDPOINT")
+            .expect("Could not get ETHEREUM_MAINNET_ENDPOINT");
+        let middleware = Arc::new(Provider::<Http>::try_from(rpc_endpoint).unwrap());
+
+        let mut pool = UniswapV3Pool {
+            address: H160::from_str("0x88e6A0c2dDD26FEEb64F039a2c41296FcB3f5640").unwrap(),
+            ..Default::default()
+        };
+
+        pool.get_pool_data(middleware.clone()).await.unwrap();
+
+        let block_pool = IUniswapV3Pool::new(
+            H160::from_str("0x88e6A0c2dDD26FEEb64F039a2c41296FcB3f5640").unwrap(),
+            middleware.clone(),
+        );
+
+        let sqrt_price = block_pool.slot_0().block(16515398).call().await.unwrap().0;
+        pool.sqrt_price = sqrt_price;
+
+        let (r_0, r_1)=pool.calculate_virtual_reserves();
+
+        
+
+        assert_eq!(1352305901809494370875_u128, r_0);
+        assert_eq!(804747469643711555_u128, r_1);
+        
+    }
 
     #[tokio::test]
     async fn test_calculate_price() {
