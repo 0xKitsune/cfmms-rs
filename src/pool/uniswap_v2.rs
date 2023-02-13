e5a3086af3d87913efd798feeda6e8adcdc0a9ee
Evan-Kim2028 <ekcopersonal@gmail.com>
2023-02-13 15:42:46 -0500

    update gitignore

--- src/pool/uniswap_v2.rs
+++ src/pool/uniswap_v2.rs
@@ -15,8 +15,6 @@ use serde::{Deserialize, Serialize};
 
 use super::fixed_point_math::{self};
 
-use serde::{Deserialize, Serialize};
-
 pub const SYNC_EVENT_SIGNATURE: H256 = H256([
     28, 65, 30, 154, 150, 224, 113, 36, 28, 47, 33, 247, 114, 107, 23, 174, 137, 227, 202, 180,
     199, 139, 229, 14, 6, 43, 3, 169, 255, 251, 186, 209,
