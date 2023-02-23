fa29207d4041b989256f4f1618ac6a1c50b194c5
0xKitsune <0xKitsune@protonmail.com>
2023-02-23 16:59:57 -0500

    created v3 tick info batch request

---
+++ contracts/uniswap_v3/GetUniswapV3TickInfoBatchRequest.sol
@@ -0,0 +1,10 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+/**
+ @dev This contract is not meant to be deployed. Instead, use a static call with the
+      deployment bytecode as payload.
+ */
+contract GetUniswapV3TickInfoBatchRequest {
+
+}
