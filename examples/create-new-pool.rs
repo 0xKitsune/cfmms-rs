28943643b56761ecad47b09776a3913ab65fa77d
Evan-Kim2028 <ekcopersonal@gmail.com>
2023-05-25 12:49:43 -0400

    refactor(new-pool): use ? for error handling

--- examples/create-new-pool.rs
+++ examples/create-new-pool.rs
@@ -12,11 +12,11 @@ async fn main() -> Result<(), Box<dyn Error>> {
     //Add rpc endpoint here:
     let rpc_endpoint = std::env::var("ETHEREUM_MAINNET_ENDPOINT")
         .expect("Could not get ETHEREUM_MAINNET_ENDPOINT");
-    let provider = Arc::new(Provider::<Http>::try_from(rpc_endpoint).unwrap());
+    let provider = Arc::new(Provider::<Http>::try_from(rpc_endpoint)?);
 
     //UniswapV2 usdc weth pool on Eth mainnet
     let _uniswap_v2_usdc_weth_pool = Pool::new_from_address(
-        H160::from_str("0xB4e16d0168e52d35CaCD2c6185b44281Ec28C9Dc").unwrap(),
+        H160::from_str("0xB4e16d0168e52d35CaCD2c6185b44281Ec28C9Dc")?,
         DexVariant::UniswapV2,
         provider.clone(),
     )
@@ -24,7 +24,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
 
     //UniswapV3 usdc weth pool on Eth mainnet
     let _uniswap_v3_usdc_weth_pool = Pool::new_from_address(
-        H160::from_str("0x88e6A0c2dDD26FEEb64F039a2c41296FcB3f5640").unwrap(),
+        H160::from_str("0x88e6A0c2dDD26FEEb64F039a2c41296FcB3f5640")?,
         DexVariant::UniswapV3,
         provider.clone(),
     )
