bcd92c12cc9348bcc5c5c5a5b4456ec34dde1258
Evan-Kim2028 <ekcopersonal@gmail.com>
2023-05-25 12:52:21 -0400

    refactor(sync-pairs): use ? for error handling

--- examples/sync-pairs.rs
+++ examples/sync-pairs.rs
@@ -13,28 +13,27 @@ use cfmms::{
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     //Add rpc endpoint here:
-    let rpc_endpoint = std::env::var("ETHEREUM_MAINNET_ENDPOINT")
-        .expect("Could not get ETHEREUM_MAINNET_ENDPOINT");
-    let provider = Arc::new(Provider::<Http>::try_from(rpc_endpoint).unwrap());
+    let rpc_endpoint = std::env::var("ETHEREUM_MAINNET_ENDPOINT")?;
+    let provider = Arc::new(Provider::<Http>::try_from(rpc_endpoint)?);
 
     let dexes = vec![
         //UniswapV2
         Dex::new(
-            H160::from_str("0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f").unwrap(),
+            H160::from_str("0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f")?,
             DexVariant::UniswapV2,
             2638438,
             Some(300),
         ),
         //Add Sushiswap
         Dex::new(
-            H160::from_str("0xC0AEe478e3658e2610c5F7A4A2E1777cE9e4f2Ac").unwrap(),
+            H160::from_str("0xC0AEe478e3658e2610c5F7A4A2E1777cE9e4f2Ac")?,
             DexVariant::UniswapV2,
             10794229,
             Some(300),
         ),
         //Add UniswapV3
         Dex::new(
-            H160::from_str("0x1F98431c8aD98523631AE4a59f267346ea31F984").unwrap(),
+            H160::from_str("0x1F98431c8aD98523631AE4a59f267346ea31F984")?,
             DexVariant::UniswapV3,
             12369621,
             None,
